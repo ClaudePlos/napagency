cd6b1ab38708971e7d815561025cdb69be57b790
k.skowronski <claude-plos@o2.pl>
2022-02-11 10:48:50 +0100

    Initial commit

---
+++ src/main/java/pl/kskowronski/Application.java
@@ -0,0 +1,29 @@
+package pl.kskowronski;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "napagency")
+@PWA(name = "nap_agency", shortName = "nap_agency", offlineResources = {"images/logo.png"})
+@Push
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
